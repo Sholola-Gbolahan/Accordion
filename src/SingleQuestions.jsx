f1f7d2fce95783597d2172ae74192cd3d751c000
Gbolahan <shololagbolahan@gmail.com>
2024-06-05 16:08:20 +0100

    Auto Functional Implemented

--- src/SingleQuestions.jsx
+++ src/SingleQuestions.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
-const SingleQuestions = ({ title, info }) => {
-  const [showInfo, setShowInfo] = useState(false);
+const SingleQuestions = ({ id, title, info, activeId, toggleQuestion }) => {
+  const isActive = id === activeId;
 
   return (
     <article className="question">
@@ -11,13 +11,13 @@ const SingleQuestions = ({ title, info }) => {
         <button
           className="question-btn"
           onClick={() => {
-            setShowInfo(!showInfo);
+            toggleQuestion(id);
           }}
         >
-          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+          {isActive ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
-      {showInfo && <p>{info}</p>}
+      {isActive && <p>{info}</p>}
     </article>
   );
 };
